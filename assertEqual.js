628206989d06db495c0569c4df536af674b7659f
ajstanley88@gmail.com <ajstanley88@gmail.com>
2022-01-11 22:21:45 -0500

    Updated assertEqual witch correct syntax

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,27 @@
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`☑️ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const tail = function (list) {
+  let answer = list.splice(1);
+  return answer;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);

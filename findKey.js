9bc651e96b4b5d1babdb58141b9a4e96f538a484
ajstanley88@gmail.com <ajstanley88@gmail.com>
2022-01-16 17:21:16 -0500

    refactored and discovered using the .entires method to iterate so I can use a for..of loop.) '

---
+++ findKey.js
@@ -0,0 +1,18 @@
+// Implement the function findKey which takes in an object and a callback.
+// scan the object and return the first key for which the callback returns a truthy value.
+// no key is found, then it should return undefined.
+
+const findKey = (obj, fn) =>
+  Object.keys(obj).find((key) => fn(obj[key], key, obj));
+
+findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+);
